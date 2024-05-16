d4370d75998336ec3b6241edd8ddaacb48d9f4eb
Betelhem Negash <119295788+BettyyN@users.noreply.github.com>
2024-05-16 21:30:05 +0300

    Updated Longest Repeating Character Replacement.

--- Longest Repeating Character Replacement.py
+++ Longest Repeating Character Replacement.py
@@ -1,16 +1,23 @@
 class Solution:
     def characterReplacement(self, s: str, k: int) -> int:
-        max_length = 0
+        if not s:
+            return 0
         max_count = 0
-        start = 0
-        count_map = [0] * 26  # Assuming only uppercase English characters
-        for end in range(len(s)):
-            index = ord(s[end]) - ord('A')
-            count_map[index] += 1
-            max_count = max(max_count, count_map[index])
-            while end - start + 1 - max_count > k:
-                count_map[ord(s[start]) - ord('A')] -= 1
-                start += 1
-            max_length = max(max_length, end - start + 1)
+        max_length = 0
+        count = {}
+        left = 0
+        for right in range(len(s)):
+            count[s[right]] = count.get(s[right], 0) + 1
+            max_count = max(max_count, count[s[right]])
+            while (right - left + 1) - max_count > k:
+                count[s[left]] -= 1
+                left += 1
+            max_length = max(max_length, right - left + 1)
+
         return max_length
+                    
+                
+            
+                
+                
                 
